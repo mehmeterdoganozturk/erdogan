61f1d6a17d290e3907c787371c465836c64c4322
erdogan <mehmeterdoganozturk@gmail.com>
2019-04-03 09:42:22 +0300

    Merge branch 'master' of https://github.com/mehmeterdoganozturk/erdogan
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/workspace.xml

--- App2.py
+++ App2.py
@@ -17,3 +17,51 @@ if(len(records) > 0):
         soru1 = soru1+str(record[0])+". "+record[2]+" - "+record[1]+"\n"
     soru1 = soru1+"\nçıkmak için q"
     print(soru1)
+
+while True:
+    binaid =input("Bina Seç")
+    if(işlem=="q"):
+        print("program sonlanıyor")
+        break
+    elif (binaid in)
+    elif (işlem == "1"):
+        Kutuphane.kitapları_goster()
+    elif (işlem == "2"):
+        isim=input("hangi kitap?")
+        print("kitap soruluyo")
+        time.sleep(2)
+        Kutuphane.kitap_sorgula(isim)
+    elif (işlem == "3"):
+        isim=input("kitap ismi:")
+        yazar=input("yazar:")
+        yayınevi=input("yayınevi:")
+        tür=input("türü:")
+        baskı=int(input("baskı:"))
+
+        yeni_kitap=kitap(isim,yazar,yayınevi,tür,baskı)
+        print("kitap ekleniyor")
+        time.sleep(2)
+        Kutuphane.kitap_ekle(yeni_kitap)
+        print("kitep eklendi")
+
+
+    elif (işlem == "4"):
+        isim= input("hangi kitap silinecek")
+
+        cevap=input("emin misin? E/H")
+        if (cevap=="E"):
+            print("kitap siliniyor")
+            time.sleep(2)
+            Kutuphane.kitap_sil(isim)
+            print("kitap silindi")
+
+
+    elif (işlem == "5"):
+        isim=input("hangi kitap baskısı yükselcek")
+        print("baskı yükseltiliyor")
+        time.sleep(2)
+        Kutuphane.baskı_yukselt(isim)
+        print("yükseltildi")
+    else:
+        print("geçersiz")
+
