ecf79d255f6fa6b3c54f0b2b5c7f5fd7c13aec63
erdogan <mehmeterdoganozturk@gmail.com>
2019-04-03 09:46:45 +0300

    Merge branch 'master' of https://github.com/mehmeterdoganozturk/erdogan
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/workspace.xml

--- App2.py
+++ App2.py
@@ -11,58 +11,22 @@ dbclass = querytrigger()
 
 sql = "SELECT backbone.id as id, backbone.ip as ip, bina.ad as binaad,bina.id as binaid FROM backbone INNER JOIN bina ON (backbone.binaid = bina.id) WHERE backbone.switchkontrol = 1 ORDER BY backbone.id ASC"
 records = dbclass.simpleselectquery(sql)
-binalist = []
+backboneidlist = []
 if(len(records) > 0):
     for record in records:
         soru1 = soru1+str(record[0])+". "+record[2]+" - "+record[1]+"\n"
-        binalist.append()
+        backboneidlist.append(record[0])
+
     soru1 = soru1+"\nçıkmak için q"
     print(soru1)
 
 while True:
-    binaid =input("Bina Seç")
-    if(işlem=="q"):
+    backboneid =input("Bina Seç")
+    if(islem=="q"):
         print("program sonlanıyor")
         break
-    elif (binaid in)
-    elif (işlem == "1"):
-        Kutuphane.kitapları_goster()
-    elif (işlem == "2"):
-        isim=input("hangi kitap?")
-        print("kitap soruluyo")
-        time.sleep(2)
-        Kutuphane.kitap_sorgula(isim)
-    elif (işlem == "3"):
-        isim=input("kitap ismi:")
-        yazar=input("yazar:")
-        yayınevi=input("yayınevi:")
-        tür=input("türü:")
-        baskı=int(input("baskı:"))
-
-        yeni_kitap=kitap(isim,yazar,yayınevi,tür,baskı)
-        print("kitap ekleniyor")
-        time.sleep(2)
-        Kutuphane.kitap_ekle(yeni_kitap)
-        print("kitep eklendi")
-
-
-    elif (işlem == "4"):
-        isim= input("hangi kitap silinecek")
-
-        cevap=input("emin misin? E/H")
-        if (cevap=="E"):
-            print("kitap siliniyor")
-            time.sleep(2)
-            Kutuphane.kitap_sil(isim)
-            print("kitap silindi")
-
-
-    elif (işlem == "5"):
-        isim=input("hangi kitap baskısı yükselcek")
-        print("baskı yükseltiliyor")
-        time.sleep(2)
-        Kutuphane.baskı_yukselt(isim)
-        print("yükseltildi")
+    elif (backboneid in backboneidlist)
+        print("içinde")
     else:
         print("geçersiz")
 
