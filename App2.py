59e308a8e9eb60ef015cdcc746c6f64aad1d88d4
erdogan <mehmeterdoganozturk@gmail.com>
2019-04-03 09:44:05 +0300

    Merge branch 'master' of https://github.com/mehmeterdoganozturk/erdogan
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/workspace.xml

--- App2.py
+++ App2.py
@@ -11,10 +11,11 @@ dbclass = querytrigger()
 
 sql = "SELECT backbone.id as id, backbone.ip as ip, bina.ad as binaad,bina.id as binaid FROM backbone INNER JOIN bina ON (backbone.binaid = bina.id) WHERE backbone.switchkontrol = 1 ORDER BY backbone.id ASC"
 records = dbclass.simpleselectquery(sql)
-
+binalist = []
 if(len(records) > 0):
     for record in records:
         soru1 = soru1+str(record[0])+". "+record[2]+" - "+record[1]+"\n"
+        binalist.append()
     soru1 = soru1+"\nçıkmak için q"
     print(soru1)
 
